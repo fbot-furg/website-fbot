c22b360108c0db58941ab7fe6ce456c0b913c401
Andre Maurell <andremaurell3@furg.br>
2023-11-26 22:58:30 -0300

    2.0 version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import react from '@astrojs/react';
+import solidJs from '@astrojs/solid-js';
 
-// https://astro.build/config
-import react from "@astrojs/react";
 
 // https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
-  integrations: [react({
-    include: ['**/react/*']
-  }), tailwind(), image({
+  integrations: [react(), solidJs(), tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
   })],
   vite: {
